69ed573a7090159c39c5ff27cc612dcb4e624bfc
CHARAN <132073925+charan-avasoft@users.noreply.github.com>
2024-02-28 18:15:34 +0530

    Added the comment line for run app

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'App.dart';
 /* Outline the main function*/
 /* Define main function*/
 void main(List<String> args) {
+  // Run the main part of our app, which is defined by the MyApp widget. 
+  // We use the 'const' keyword to make sure the MyApp widget is only created once, 
+  // which helps our app run more efficiently.
   runApp(const MyApp());
 }
 
