b1e69805064393a30ab4db5fce26acfee379b839
Sriram Srinivas <sriram.s.r@avasoft.com>
2024-02-28 18:17:25 +0530

    produce conflict

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main(List<String> args) {
   // Run the main part of our app, which is defined by the MyApp widget. 
   // We use the 'const' keyword to make sure the MyApp widget is only created once, 
   // which helps our app run more efficiently.
+  // Runapp
   runApp(const MyApp());
 }
 
